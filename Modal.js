9f85c1e2bfb382080ed6f05cce8ae518e9595fa4
Jakub Piasecki <jakub.piasecki@swmansion.com>
2023-12-02 14:40:06 +0100

    Setup scroll with pan

--- Modal.js
+++ Modal.js
@@ -1,5 +1,5 @@
 import { useCallback, useState } from "react";
-import { Pressable, ScrollView, View, Text } from "react-native";
+import { Pressable, View, Text } from "react-native";
 import Animated, {
   SlideInDown,
   SlideOutDown,
@@ -10,6 +10,9 @@ import Animated, {
   withSpring,
   withTiming,
   runOnJS,
+  useAnimatedRef,
+  useScrollViewOffset,
+  useAnimatedProps,
 } from "react-native-reanimated";
 import {
   RectButton,
@@ -26,6 +29,12 @@ export default function Modal({ toggleSheet, initialGenres, setGenres }) {
   const [currentGenres, setCurrentGenres] = useState(initialGenres);
   const offset = useSharedValue(0);
 
+  const scrollEnabled = useSharedValue(true);
+  const lastPositionY = useSharedValue(0);
+
+  const animatedScrollViewRef = useAnimatedRef();
+  const scrollHandler = useScrollViewOffset(animatedScrollViewRef);
+
   const save = useCallback(() => {
     setGenres(currentGenres);
     toggleSheet();
@@ -35,7 +44,17 @@ export default function Modal({ toggleSheet, initialGenres, setGenres }) {
     transform: [{ translateY: offset.value }],
   }));
 
+  const scrollProps = useAnimatedProps(() => ({
+    scrollEnabled: scrollEnabled.value,
+  }));
+
+  const scrollGesture = Gesture.Native();
+
   const pan = Gesture.Pan()
+    .manualActivation(true)
+    .onStart(() => {
+      scrollEnabled.value = false;
+    })
     .onChange((event) => {
       const offsetDelta = event.changeY + offset.value;
 
@@ -43,6 +62,8 @@ export default function Modal({ toggleSheet, initialGenres, setGenres }) {
       offset.value = offsetDelta > 0 ? offsetDelta : withSpring(clamp);
     })
     .onFinalize(() => {
+      scrollEnabled.value = true;
+
       if (offset.value < HEIGHT / 3) {
         offset.value = withSpring(0);
       } else {
@@ -50,7 +71,19 @@ export default function Modal({ toggleSheet, initialGenres, setGenres }) {
           runOnJS(toggleSheet)();
         });
       }
-    });
+    })
+    .onTouchesDown((e) => {
+      lastPositionY.value = e.changedTouches[0].y;
+    })
+    .onTouchesMove((e, stateManager) => {
+      const change = lastPositionY.value - e.changedTouches[0].y;
+      lastPositionY.value = e.changedTouches[0].y;
+
+      if (change < 0 && scrollHandler.value <= 0) {
+        stateManager.activate();
+      }
+    })
+    .simultaneousWithExternalGesture(scrollGesture);
 
   return (
     <>
@@ -70,17 +103,23 @@ export default function Modal({ toggleSheet, initialGenres, setGenres }) {
             <Text style={styles.modalHeaderText}>Choose</Text>
           </View>
 
-          <ScrollView>
-            {currentGenres.map((genre, index) => (
-              <GenreSelector
-                key={index}
-                genre={genre.name}
-                selected={genre.selected}
-                last={index == currentGenres.length - 1}
-                setCurrentGenres={setCurrentGenres}
-              />
-            ))}
-          </ScrollView>
+          <GestureDetector gesture={scrollGesture}>
+            <Animated.ScrollView
+              ref={animatedScrollViewRef}
+              animatedProps={scrollProps}
+              scrollEventThrottle={1}
+            >
+              {currentGenres.map((genre, index) => (
+                <GenreSelector
+                  key={index}
+                  genre={genre.name}
+                  selected={genre.selected}
+                  last={index == currentGenres.length - 1}
+                  setCurrentGenres={setCurrentGenres}
+                />
+              ))}
+            </Animated.ScrollView>
+          </GestureDetector>
 
           <RectButton
             style={[styles.button, styles.horizontalMargin]}
